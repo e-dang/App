accc88bc6cd5a3c3ce432a1ea6a651c13dcd4273
Eric Dang <edang830@gmail.com>
2021-07-19 08:18:50 -0700

    Use consistent variable brackets

--- scripts/kube-config.sh
+++ scripts/kube-config.sh
@@ -2,7 +2,7 @@
 
 CLUSTER_NAME=${1:-"tracker-dev"}
 RESOURCE_GROUP=${2:-"tracker-stack-dev"}
-kubectl config unset users.clusterAdmin_${RESOURCE_GROUP}_$CLUSTER_NAME
-kubectl config unset clusters.$CLUSTER_NAME
-kubectl config unset contexts.$CLUSTER_NAME-admin
-az aks get-credentials -n $CLUSTER_NAME --resource-group ${RESOURCE_GROUP} --admin
+kubectl config unset users.clusterAdmin_${RESOURCE_GROUP}_${CLUSTER_NAME}
+kubectl config unset clusters.${CLUSTER_NAME}
+kubectl config unset contexts.${CLUSTER_NAME}-admin
+az aks get-credentials -n ${CLUSTER_NAME} --resource-group ${RESOURCE_GROUP} --admin
