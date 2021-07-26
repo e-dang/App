b6409e4c4500dcf8dd35be53267776b4e462af06
Eric Dang <edang830@gmail.com>
2021-07-25 20:29:05 -0700

    Add unit tests for Client

--- frontend/mobile/__tests__/api/client.test.ts
+++ frontend/mobile/__tests__/api/client.test.ts
@@ -1,7 +1,56 @@
 import Client from '@api/client';
+import {AxiosInstance} from 'axios';
+import {mock} from 'jest-mock-extended';
 
-describe('test Client', () => {
-    it('registers a user', async () => {
-        expect(true);
+describe('Client', () => {
+    const url = 'https://test.url.dne/';
+    const data = {};
+
+    it('delete calls delete method on impl with url', async () => {
+        Client['impl'] = mock<AxiosInstance>();
+
+        Client.delete(url);
+
+        expect(Client['impl'].delete).toHaveBeenCalledWith(url);
+    });
+
+    it('get calls get method on impl with url', async () => {
+        Client['impl'] = mock<AxiosInstance>();
+
+        Client.get(url);
+
+        expect(Client['impl'].get).toHaveBeenCalledWith(url);
+    });
+
+    it('head calls head method on impl with url', async () => {
+        Client['impl'] = mock<AxiosInstance>();
+
+        Client.head(url);
+
+        expect(Client['impl'].head).toHaveBeenCalledWith(url);
+    });
+
+    it('post calls post method on impl with url and data', async () => {
+        Client['impl'] = mock<AxiosInstance>();
+
+        Client.post(url, data);
+
+        expect(Client['impl'].post).toHaveBeenCalledWith(url, data);
+    });
+
+    it('put calls put method on impl with url and data', async () => {
+        Client['impl'] = mock<AxiosInstance>();
+
+        Client.put(url, data);
+
+        expect(Client['impl'].put).toHaveBeenCalledWith(url, data);
+    });
+
+    it('patch calls patch method on impl with url and data', async () => {
+        Client['impl'] = mock<AxiosInstance>();
+
+        Client.patch(url, data);
+
+        expect(Client['impl'].patch).toHaveBeenCalledWith(url, data);
     });
 });
