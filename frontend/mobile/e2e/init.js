11874d50f50bd0ccabb4af7b6473e3cee3dac981
Eric Dang <edang830@gmail.com>
2021-07-25 17:39:54 -0700

    Setup e2e testing with detox

---
+++ frontend/mobile/e2e/init.js
@@ -0,0 +1,28 @@
+import detox, {device} from 'detox';
+import adapter from 'detox/runners/jest/adapter';
+import specReporter from 'detox/runners/jest/specReporter';
+import config from '../.detoxrc.json';
+
+jasmine.getEnv().addReporter(adapter);
+
+// This takes care of generating status logs on a per-spec basis. By default, jest only reports at file-level.
+// This is strictly optional.
+jasmine.getEnv().addReporter(specReporter);
+
+beforeAll(async () => {
+    await detox.init(config.detox, {launchApp: false});
+    await device.launchApp({
+        /** this is where you can set what permissions might be needed */
+        // permissions: { notifications: 'YES', camera: 'YES' },
+        newInstance: true,
+    });
+});
+
+beforeEach(async () => {
+    await adapter.beforeEach();
+});
+
+afterAll(async () => {
+    await adapter.afterAll();
+    await detox.cleanup();
+});
