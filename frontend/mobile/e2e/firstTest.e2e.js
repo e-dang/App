d029794d3e076b88ab568e2a9cab44f60a0268ad
Eric Dang <edang830@gmail.com>
2021-07-31 10:13:37 -0700

    Add email registration e2e test

--- frontend/mobile/e2e/firstTest.e2e.js
+++ frontend/mobile/e2e/firstTest.e2e.js
@@ -1,4 +1,4 @@
-describe('Example', () => {
+describe('Auth flow', () => {
     beforeAll(async () => {
         await device.launchApp();
     });
@@ -7,20 +7,38 @@ describe('Example', () => {
         await device.reloadReactNative();
     });
 
-    it('should have an email text input', async () => {
-        await expect(element(by.id('email'))).toBeVisible();
+    test('Register screen should have an name, email, password text input and a sign up button', async () => {
+        await expect(element(by.id('nameInput'))).toBeVisible();
+        await expect(element(by.id('emailInput'))).toBeVisible();
+        await expect(element(by.id('passwordInput'))).toBeVisible();
+        await expect(element(by.id('signUpBtn'))).toBeVisible();
     });
-    //   it('should have welcome screen', async () => {
-    //     await expect(element(by.id('welcome'))).toBeVisible();
-    //   });
 
-    //   it('should show hello screen after tap', async () => {
-    //     await element(by.id('hello_button')).tap();
-    //     await expect(element(by.text('Hello!!!'))).toBeVisible();
-    //   });
+    test('When a user successfully registers they arent taken to the home screen', async () => {
+        const name = 'Test User';
+        const email = 'demo@example.com';
+        const password = 'mytestpassword123';
 
-    //   it('should show world screen after tap', async () => {
-    //     await element(by.id('world_button')).tap();
-    //     await expect(element(by.text('World!!!'))).toBeVisible();
-    //   });
+        // the user enters their name, email, and password
+        await element(by.id('nameInput')).typeText(name);
+        await element(by.id('emailInput')).typeText(email);
+        await element(by.id('passwordInput')).typeText(password);
+
+        await expect(element(by.id('nameInput'))).toHaveText(name);
+        await expect(element(by.id('emailInput'))).toHaveText(email);
+        await expect(element(by.id('passwordInput'))).toHaveText(password);
+
+        // the user then hits the Sign Up button
+        await element(by.id('signUpBtn')).tap();
+
+        // they see a loading icon appear
+        await waitFor(element(by.id('loadingModal')))
+            .toBeVisible()
+            .withTimeout(2000);
+
+        // the registration is successful, and they are taken to the home page
+        await waitFor(element(by.id('home')))
+            .toBeVisible()
+            .withTimeout(2000);
+    });
 });
