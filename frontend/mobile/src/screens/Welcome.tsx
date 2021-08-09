7e8de34b7eada452d6c94cf45f7ce86e818ab751
Eric Dang <edang830@gmail.com>
2021-08-08 21:02:10 -0700

    Fix sign up button text

--- frontend/mobile/src/screens/Welcome.tsx
+++ frontend/mobile/src/screens/Welcome.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Center, Button, Icon as NativeIcon, Stack, Box, Heading, Text, Row} from 'native-base';
+import {Center, Button, Icon as NativeIcon, Stack, Box, Heading, Text, Row, Divider} from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import {Screen} from '@components';
 import {NavigationService} from '@utils';
@@ -15,17 +15,17 @@ export function Welcome() {
 
     return (
         <Screen testID="welcomeScreen">
-            <Center flex={2}>
+            <Center flex={4} justifyContent="flex-start">
                 <Heading>Welcome!</Heading>
             </Center>
-            <Center flex={4}>
+            <Center flex={3}>
                 <Stack direction="column" space={2} width="90%">
                     <Button
                         borderRadius={100}
                         testID="emailSignUpBtn"
                         onPress={handleSignUpWithEmail}
                         startIcon={<NativeIcon as={Icon} name="envelope" size={5} />}>
-                        Sign In With Email
+                        Sign Up With Email
                     </Button>
                     <Button
                         borderRadius={100}
@@ -34,7 +34,7 @@ export function Welcome() {
                         colorScheme="secondary"
                         onPress={() => console.log('Apple')}
                         startIcon={<NativeIcon as={Icon} name="apple" size={5} color="black" />}>
-                        Sign In With Apple
+                        Sign Up With Apple
                     </Button>
                     <Button
                         borderRadius={100}
@@ -43,7 +43,7 @@ export function Welcome() {
                         colorScheme="secondary"
                         onPress={() => console.log('Facebook')}
                         startIcon={<NativeIcon as={Icon} name="facebook" size={5} color="black" />}>
-                        Sign In With Facebook
+                        Sign Up With Facebook
                     </Button>
                     <Button
                         borderRadius={100}
@@ -52,9 +52,10 @@ export function Welcome() {
                         colorScheme="secondary"
                         onPress={() => console.log('Google')}
                         startIcon={<NativeIcon as={Icon} name="google" size={5} color="black" />}>
-                        Sign In With Google
+                        Sign Up With Google
                     </Button>
-                    <Box mt={7}>
+                    <Divider my={3} />
+                    <Box>
                         <Text alignSelf="center">Already have an account?</Text>
                         <Button testID="signInBtn" variant="ghost" onPress={handleSignIn}>
                             Sign In
