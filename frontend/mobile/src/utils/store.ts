24fa4d5c49033bdff8f43c6694ba8762fbd50828
Eric Dang <edang830@gmail.com>
2021-08-08 17:37:22 -0700

    Remove getPending utility function

--- frontend/mobile/src/utils/store.ts
+++ frontend/mobile/src/utils/store.ts
@@ -4,10 +4,3 @@ import {RootAction} from '@actions';
 import {RootState} from '@src/store';
 
 export const useSelector = createSelectorHook<RootState, RootAction>();
-
-export function getPending(obj: PendingState, prop: keyof PendingState) {
-    if (obj[prop] === undefined) {
-        return false;
-    }
-    return obj[prop].pending;
-}
