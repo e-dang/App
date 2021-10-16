547640c3c35801d15811899dc7e94e6cbc970034
Eric Dang <edang830@gmail.com>
2021-10-16 15:48:25 -0700

    Use pulumi for identity infra management

---
+++ infra/identity/index.ts
@@ -0,0 +1,38 @@
+import * as pulumi from '@pulumi/pulumi';
+import * as azure from '@pulumi/azure-native';
+import * as azuread from '@pulumi/azuread';
+
+interface User extends azuread.UserArgs {
+    groups: string[];
+}
+
+const config = new pulumi.Config();
+const usersRaw = config.requireObject<User[]>('users');
+const currentPrincipal = azure.authorization.getClientConfig().then((current) => current.objectId);
+
+const users = usersRaw.map((user) => ({
+    userObj: new azuread.User('user', {
+        userPrincipalName: user.userPrincipalName,
+        displayName: user.displayName,
+        password: user.password,
+    }),
+    groups: user.groups,
+}));
+
+const admins = new azuread.Group('admins', {
+    displayName: 'admins',
+    members: [
+        currentPrincipal,
+        ...users.filter((user) => user.groups.includes('admins')).map((user) => user.userObj.objectId),
+    ],
+    securityEnabled: true,
+});
+
+const devs = new azuread.Group('devs', {
+    displayName: 'devs',
+    members: users.filter((user) => user.groups.includes('devs')).map((user) => user.userObj.objectId),
+    securityEnabled: true,
+});
+
+export const adGroupAdmins = admins.objectId;
+export const adGroupDevs = devs.objectId;
