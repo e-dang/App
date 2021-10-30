d9afa068072ca0c05f9af71300b5ed1696a3c54a
Eric Dang <edang830@gmail.com>
2021-10-30 15:19:28 -0700

    Default to keys of decrypted file being secrets

--- infra/utils.ts
+++ infra/utils.ts
@@ -1,6 +1,25 @@
+import * as pulumi from '@pulumi/pulumi';
 import {execSync} from 'child_process';
+import {load} from 'js-yaml';
 
-export function decryptSopsFile(filepath: string) {
-    const output = execSync(`sops -d ${filepath}`).toString();
-    return output;
+interface RawDecryptedSecret {
+    [x: string]: string;
+}
+
+// Work around of typescript stupidity
+// https://stackoverflow.com/questions/50321419/typescript-returntype-of-generic-function/64840774#answer-64840774
+export const wrapperPulumiSecret = (val: pulumi.Input<string>) => pulumi.secret<string>(val);
+export type PulumiSecret = ReturnType<typeof wrapperPulumiSecret>;
+
+export interface DecryptedSecret {
+    [x: string]: PulumiSecret;
+}
+
+export function decryptSopsFile<T extends DecryptedSecret>(filepath: string) {
+    const yamlOutput = load(execSync(`sops -d ${filepath}`).toString()) as RawDecryptedSecret;
+    const secretOutput: DecryptedSecret = {};
+    for (const key in yamlOutput) {
+        secretOutput[key] = pulumi.secret(yamlOutput[key]);
+    }
+    return secretOutput as T;
 }
