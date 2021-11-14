9cfb633328a5e4d2362826c3334a9fb283a80c9d
Eric Dang <edang830@gmail.com>
2021-11-14 12:03:49 -0800

    Remove custom routing and use express router

--- backend/auth/src/index.ts
+++ backend/auth/src/index.ts
@@ -1,15 +1,15 @@
 import 'reflect-metadata';
-import {settings} from '../config';
+import {config} from '@config';
 import {createConnection} from 'typeorm';
 import * as express from 'express';
-import * as bodyParser from 'body-parser';
-import {Request, Response} from 'express';
-import {Routes} from './routes';
-import {User} from './entity/User';
+import {User} from '@entities';
+import * as passport from 'passport';
+import {strategy} from './passport';
+import {authRouter, userRouter} from '@api';
 
 createConnection({
     type: 'postgres', // WHY MUST I HARDCODE STUPID ASS TYPESCRIPT
-    url: settings.dbUrl,
+    url: config.dbUrl,
     synchronize: true,
     logging: false,
     entities: [User],
@@ -21,44 +21,21 @@ createConnection({
     },
 })
     .then(async (connection) => {
-        // create express app
+        // intialize components
         const app = express();
-        app.use(bodyParser.json());
+        passport.use(strategy);
 
-        // register express routes from defined application routes
-        Routes.forEach((route) => {
-            (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-                const result = new (route.controller as any)()[route.action](req, res, next);
-                if (result instanceof Promise) {
-                    result.then((result) => (result !== null && result !== undefined ? res.send(result) : undefined));
-                } else if (result !== null && result !== undefined) {
-                    res.json(result);
-                }
-            });
-        });
+        // middleware
+        app.use(express.json());
+        app.use(passport.initialize());
 
-        // setup express app here
-        // ...
+        // add apis
+        app.use(authRouter);
+        app.use(userRouter);
 
         // start express server
-        app.listen(3000);
+        app.listen(config.httpPort);
 
-        // insert new users for test
-        // await connection.manager.save(
-        //     connection.manager.create(User, {
-        //         firstName: 'Timber',
-        //         lastName: 'Saw',
-        //         age: 27,
-        //     }),
-        // );
-        // await connection.manager.save(
-        //     connection.manager.create(User, {
-        //         firstName: 'Phantom',
-        //         lastName: 'Assassin',
-        //         age: 24,
-        //     }),
-        // );
-
-        console.log('Express server has started on port 3000. Open http://localhost:3000/users to see results');
+        console.log('Express server has started on port 3000');
     })
     .catch((error) => console.log(error));
