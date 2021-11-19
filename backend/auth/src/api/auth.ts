7be5b30a24d8428f4de1b3c2a5f2a321964d90ea
Eric Dang <edang830@gmail.com>
2021-11-18 23:53:08 -0800

    Throw custom errors

--- backend/auth/src/api/auth.ts
+++ backend/auth/src/api/auth.ts
@@ -6,7 +6,7 @@ import {body} from 'express-validator';
 import {PG_UNIQUE_CONSTRAINT_VIOLATION} from '@src/constants';
 import passport from 'passport';
 import {validate} from './schema';
-import {SignInError} from '@src/errors';
+import {ExpiredTokenError, SignInError, UserAlreadyExistsError, UserNotFoundError} from '@src/errors';
 
 const authRouter = Router();
 
@@ -42,13 +42,13 @@ authRouter.post(
 authRouter.post(
     '/signup',
     validate([body('name').not().isEmpty(), body('email').isEmail(), body('password').isStrongPassword()]),
-    async (req: Request, res: Response) => {
+    async (req: Request, res: Response, next: NextFunction) => {
         let user: User;
         try {
             user = await User.createUser(req.body.name, req.body.email, req.body.password);
         } catch (err) {
             if (err.detail.includes('(email)=') && err.code == PG_UNIQUE_CONSTRAINT_VIOLATION) {
-                return res.status(409).json({error: 'A user with that email has already been registered.'});
+                return next(new UserAlreadyExistsError(req.body.email));
             }
         }
 
@@ -62,24 +62,28 @@ authRouter.post('/password/reset/confirm', async (req: Request, res: Response) =
 
 authRouter.post('/password/change', async (req: Request, res: Response) => {});
 
-authRouter.post('/token/refresh', validate([body('refreshToken').isJWT()]), async (req: Request, res: Response) => {
-    let payload: any;
-    try {
-        payload = verifyRefreshToken(req.body.refreshToken);
-    } catch (err) {
-        return res.status(400).json({errors: 'This jwt is no longer valid'});
-    }
+authRouter.post(
+    '/token/refresh',
+    validate([body('refreshToken').isJWT()]),
+    async (req: Request, res: Response, next: NextFunction) => {
+        let payload: any;
+        try {
+            payload = verifyRefreshToken(req.body.refreshToken);
+        } catch (err) {
+            return next(new ExpiredTokenError());
+        }
 
-    const user = await User.findOne({id: payload.userId});
+        const user = await User.findOne({id: payload.userId});
 
-    if (!user) {
-        return res.status(400).json({errors: 'This user no longer exists.'});
-    } else if (user.tokenVersion != payload.tokenVersion) {
-        return res.status(400).json({errors: 'This token has been revoked.'});
-    }
+        if (!user) {
+            return next(new UserNotFoundError(payload.userId));
+        } else if (user.tokenVersion != payload.tokenVersion) {
+            return next(new ExpiredTokenError());
+        }
 
-    return res.status(200).json(createJwt(user));
-});
+        return res.status(200).json(createJwt(user));
+    },
+);
 
 export const authApis: ApiGroup = {
     pathPrefix: 'auth',
--- backend/auth/src/errors.ts
+++ backend/auth/src/errors.ts
@@ -25,14 +25,37 @@ export class InternalError extends Error {
     }
 }
 
+export class ExpiredTokenError extends InternalError {
+    constructor() {
+        super(400, 'Token expired.');
+    }
+}
 export class ResourceNotFound extends InternalError {
     constructor(message: string) {
         super(404, message);
     }
 }
 
+export class UserNotFoundError extends ResourceNotFound {
+    constructor(id: string) {
+        super(`A user with the id '${id}' does not exist.`);
+    }
+}
+
 export class SignInError extends ResourceNotFound {
     constructor() {
         super('A user with this email and/or password does not exist.');
+    }
+}
+
+export class AlreadyExistsError extends InternalError {
+    constructor(message: string) {
+        super(409, message);
+    }
+}
+
+export class UserAlreadyExistsError extends AlreadyExistsError {
+    constructor(email: string) {
+        super(`A user with the email '${email}' already exists.`);
     }
 }
