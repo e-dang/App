756686a5f4d3f13b28bda56f84a19707bdf26035
Eric Dang <edang830@gmail.com>
2021-11-18 19:27:04 -0800

    Implement AuthenticatedRequest interface

--- backend/auth/src/api/user.ts
+++ backend/auth/src/api/user.ts
@@ -1,6 +1,6 @@
 import {User} from '@entities';
-import {NextFunction, Request, Response, Router} from 'express';
-import {ApiGroup} from './types';
+import {Response, Router} from 'express';
+import {ApiGroup, AuthenticatedRequest} from './types';
 import passport from 'passport';
 
 const userRouter = Router();
@@ -8,8 +8,8 @@ const userRouter = Router();
 userRouter.use(passport.authenticate('jwt', {session: false}));
 
 // get auth user
-userRouter.get('/', async (req: Request, res: Response) => {
-    const user = await User.findOne({id: (req.user as User).id});
+userRouter.get('/', async (req: AuthenticatedRequest, res: Response) => {
+    const user = await User.findOne({id: req.user.id});
     if (!user) {
         return res.status(404).json({error: 'That user no longer exists.'});
     }
@@ -18,28 +18,18 @@ userRouter.get('/', async (req: Request, res: Response) => {
 });
 
 // update auth user
-userRouter.patch('/', async (req: Request, res: Response) => {
-    const user = await User.findOne({id: (req.user as User).id});
-    if (!user) {
-        return res.status(404).json({error: 'That user no longer exists.'});
-    }
+userRouter.patch('/', async (req: AuthenticatedRequest, res: Response) => {
+    req.user.name = req.body.name || req.user.name;
+    req.user.email = req.body.email || req.user.email;
+    await req.user.save();
 
-    user.name = req.body.name || user.name;
-    user.email = req.body.email || user.email;
-    await user.save();
-
-    return res.status(200).json({data: user.serialize()});
+    return res.status(200).json({data: req.user.serialize()});
 });
 
 // delete auth user
-userRouter.delete('/', async (req: Request, res: Response) => {
-    const user = await User.findOne({id: (req.user as User).id});
-    if (!user) {
-        return res.status(404).json({error: 'That user no longer exists.'});
-    }
-
-    user.isActive = false;
-    await user.save();
+userRouter.delete('/', async (req: AuthenticatedRequest, res: Response) => {
+    req.user.isActive = false;
+    await req.user.save();
 
     return res.status(202).json();
 });
