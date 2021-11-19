155640db2aa7c1cf4a6836699aa4ddb452106f75
Eric Dang <edang830@gmail.com>
2021-11-18 23:53:22 -0800

    Remove pointless fetch from database

--- backend/auth/src/api/user.ts
+++ backend/auth/src/api/user.ts
@@ -9,12 +9,7 @@ userRouter.use(passport.authenticate('jwt', {session: false}));
 
 // get auth user
 userRouter.get('/', async (req: AuthenticatedRequest, res: Response) => {
-    const user = await User.findOne({id: req.user.id});
-    if (!user) {
-        return res.status(404).json({error: 'That user no longer exists.'});
-    }
-
-    return res.status(200).json({data: user.serialize()});
+    return res.status(200).json({data: req.user.serialize()});
 });
 
 // update auth user
