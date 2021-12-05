8ce6dd917cc6800d67c259c1436063282951cc15
Eric Dang <edang830@gmail.com>
2021-12-04 18:06:06 -0800

    Delete tracker backend

--- backend/tracker/tracker/tests/integration/users/test_models.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from tracker.users.models import User
-
-pytestmark = pytest.mark.django_db
-
-
-def test_user_get_absolute_url(user: User):
-    assert user.get_absolute_url() == f"/api/v1/users/{user.uuid}/"
