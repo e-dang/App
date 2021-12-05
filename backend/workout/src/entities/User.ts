e72e7ce05d3dd7572cd1a09df6c0d7709ed7d290
Eric Dang <edang830@gmail.com>
2021-12-05 12:51:26 -0800

    Add workout entity

--- backend/workout/src/entities/User.ts
+++ backend/workout/src/entities/User.ts
@@ -1,7 +1,11 @@
-import {Entity, PrimaryColumn, BaseEntity} from 'typeorm';
+import {Entity, PrimaryColumn, BaseEntity, OneToMany} from 'typeorm';
+import {Workout} from './Workout';
 
 @Entity('users')
 export class User extends BaseEntity {
     @PrimaryColumn('uuid')
     id: string;
+
+    @OneToMany(() => Workout, (workout) => workout.owner)
+    workouts: Workout[];
 }
---
+++ backend/workout/src/entities/Workout.ts
@@ -0,0 +1,14 @@
+import {BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn} from 'typeorm';
+import {User} from './User';
+
+@Entity('workouts')
+export class Workout extends BaseEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @ManyToOne(() => User, (user) => user.workouts)
+    owner: User;
+
+    @Column('varchar', {length: 255})
+    name: string;
+}
