1e636a0c42399ef04cad24526b110f2984f4109b
Eric Dang <edang830@gmail.com>
2021-12-05 18:20:44 -0800

    Implement workout detail endpoint

--- backend/workout/src/api/workoutApi.ts
+++ backend/workout/src/api/workoutApi.ts
@@ -1,4 +1,6 @@
 import {ApiGroup, AuthenticatedRequest} from '@api';
+import {WorkoutNotFoundError} from '@errors';
+import {validateWorkoutDetailRequest} from '@schemas';
 import {verifyAuthN} from '@src/middleware';
 import {Response, NextFunction, Router} from 'express';
 
@@ -10,7 +12,19 @@ workoutRouter.get('/', async (req: AuthenticatedRequest, res: Response, next: Ne
     return res.status(200).json({data: await req.user.getSerializedWorkouts()});
 });
 
-workoutRouter.get('/:workoutId', async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {});
+workoutRouter.get(
+    '/:workoutId',
+    validateWorkoutDetailRequest,
+    async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+        const workout = await req.user.getWorkout(req.params.workoutId);
+
+        if (!workout || workout.isDeleted) {
+            return next(new WorkoutNotFoundError(req.user.id, req.params.workoutId));
+        }
+
+        return res.status(200).json({data: workout.serialize()});
+    },
+);
 
 workoutRouter.post('/', async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {});
 
--- backend/workout/src/entities/User.ts
+++ backend/workout/src/entities/User.ts
@@ -9,13 +9,22 @@ export class User extends BaseEntity {
     @OneToMany(() => Workout, (workout) => workout.owner, {cascade: true})
     workouts: Workout[];
 
+    async addWorkout(entityLike: Omit<DeepPartial<Workout>, 'owner'>) {
+        return (await this.addWorkouts([entityLike]))[0];
+    }
+
     async addWorkouts(entityLike: Omit<DeepPartial<Workout>, 'owner'>[]) {
         this.workouts = entityLike.map((data) => Workout.create({owner: this, ...data}));
         await this.save();
+        return this.workouts;
     }
 
     async getSerializedWorkouts() {
         const workouts = await Workout.find({owner: this});
         return workouts.map((workout) => workout.serialize());
+    }
+
+    async getWorkout(id: string) {
+        return Workout.findOne({owner: this, id});
     }
 }
