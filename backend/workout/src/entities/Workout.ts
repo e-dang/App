bf20062a5f49590dfaff08495c10ecfb758e2016
Eric Dang <edang830@gmail.com>
2021-12-08 20:08:50 -0800

    Add createdAt and lastUpdatedAt fields to Workout

--- backend/workout/src/entities/Workout.ts
+++ backend/workout/src/entities/Workout.ts
@@ -1,10 +1,20 @@
-import {BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn} from 'typeorm';
+import {
+    BaseEntity,
+    Column,
+    Entity,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from 'typeorm';
 import {User} from './User';
 
 interface WorkoutDetail {
     id: string;
     owner: string;
     name: string;
+    createdAt: string;
+    lastUpdatedAt: string;
 }
 
 @Entity('workouts')
@@ -24,11 +34,19 @@ export class Workout extends BaseEntity {
     @Column({default: false})
     isDeleted: boolean;
 
+    @CreateDateColumn({type: 'timestamptz'})
+    createdAt: Date;
+
+    @UpdateDateColumn({type: 'timestamptz'})
+    lastUpdatedAt: Date;
+
     serialize(): WorkoutDetail {
         return {
             id: this.id,
             owner: this.ownerId,
             name: this.name,
+            createdAt: this.createdAt.toString(),
+            lastUpdatedAt: this.lastUpdatedAt.toString(),
         };
     }
 }
