ebc34759652820a116af1ede2432ce1af7d5f9bd
Eric Dang <edang830@gmail.com>
2021-12-11 10:04:37 -0800

    Add payload to createToken

--- backend/workouts/__tests__/integration/api/utils.ts
+++ backend/workouts/__tests__/integration/api/utils.ts
@@ -1,8 +1,9 @@
 import {config} from '@config';
+import {AccessTokenPayload} from '@src/middleware';
 import * as jose from 'jose';
 
-export async function createToken(userId: string) {
-    return new jose.SignJWT({userId})
+export async function createToken(payload: AccessTokenPayload) {
+    return new jose.SignJWT(payload)
         .setProtectedHeader({alg: 'EdDSA', typ: 'JWT'})
         .setIssuedAt()
         .setIssuer('dev.erickdang.com')
