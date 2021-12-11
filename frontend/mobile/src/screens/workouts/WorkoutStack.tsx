5ec5544803c38cfa5b0e06152a79fe77285d0bb5
Eric Dang <edang830@gmail.com>
2021-12-11 15:44:02 -0800

    Rename file to CreateWorkout

--- frontend/mobile/src/screens/workouts/WorkoutStack.tsx
+++ frontend/mobile/src/screens/workouts/WorkoutStack.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import {CreateWorkoutScreen} from './CreateWorkoutRoot';
+import {CreateWorkoutScreen} from './CreateWorkout';
 import {ListWorkoutsScreen} from './ListWorkouts';
 
 export type WorkoutStackParamList = {
