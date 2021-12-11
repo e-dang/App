72693beb14d8df352d367c8470e500fa4517d56b
Eric Dang <edang830@gmail.com>
2021-12-11 13:09:23 -0800

    Start e2e test for creating a workout

---
+++ frontend/mobile/__tests__/e2e/workouts.e2e.ts
@@ -0,0 +1,30 @@
+import {device, element, by, expect} from 'detox';
+import {createUser, signIn} from './utils';
+
+describe('create workout flow', () => {
+    let email: string;
+    let password: string;
+
+    beforeEach(async () => {
+        await device.reloadReactNative();
+        ({email, password} = await createUser());
+    });
+
+    test('user can create a new workout', async () => {
+        // the user signs in and goes to the workout screen
+        await signIn({email, password});
+        await element(by.id('navWorkouts')).tap();
+        await expect(element(by.id('workoutScreen'))).toBeVisible();
+
+        // they see a list of workouts and a button to add a workout
+        const workoutsList = element(by.id('workoutList'));
+        const createWorkoutBtn = element(by.id('createWorkoutBtn'));
+        await expect(workoutsList).toBeVisible();
+        await expect(createWorkoutBtn).toBeVisible();
+
+        // the user taps the add workout button and is taken to the create workout flow screen
+        await createWorkoutBtn.tap();
+        const createWorkoutFlowScreen = element(by.id('createWorkoutFlowScreen'));
+        await expect(createWorkoutFlowScreen).toBeVisible();
+    });
+});
