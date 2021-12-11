0a049b1c8338d6f93fe357b131318d295a240622
Eric Dang <edang830@gmail.com>
2021-12-11 11:25:53 -0800

    Add icons to bottom tabs

--- frontend/mobile/src/App.tsx
+++ frontend/mobile/src/App.tsx
@@ -6,6 +6,7 @@ import RNBootSplash from 'react-native-bootsplash';
 import {Home, Settings, SignUp, Welcome, SignIn, ForgotPassword} from '@screens';
 import {useSelector} from '@hooks';
 import {selectAuthToken} from '@selectors';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export type AppTabParamList = {
     home: undefined;
@@ -44,14 +45,9 @@ export const App = () => {
                 options={{
                     tabBarTestID: 'navHome',
                     tabBarLabel: t('home'),
-                    // tabBarIcon: ({focused, color, size}) => (
-                    //     <Icon
-                    //         name={focused ? 'home' : 'home-outline'}
-                    //         type="material-community"
-                    //         size={size}
-                    //         color={color}
-                    //     />
-                    // ),
+                    tabBarIcon: ({focused, color, size}) => (
+                        <Icon name={focused ? 'home' : 'home-outline'} size={size} color={color} />
+                    ),
                 }}
             />
             <Tab.Screen
@@ -60,14 +56,9 @@ export const App = () => {
                 options={{
                     tabBarTestID: 'navSettings',
                     tabBarLabel: t('settings'),
-                    // tabBarIcon: ({focused, color, size}) => (
-                    //     <Icon
-                    //         name={focused ? 'cog' : 'cog-outline'}
-                    //         type="material-community"
-                    //         size={size}
-                    //         color={color}
-                    //     />
-                    // ),
+                    tabBarIcon: ({focused, color, size}) => (
+                        <Icon name={focused ? 'cog' : 'cog-outline'} size={size} color={color} />
+                    ),
                 }}
             />
         </Tab.Navigator>
