bb592fd46e3a668c9e6ab1174848279fd18abb00
Eric Dang <edang830@gmail.com>
2021-12-12 17:34:10 -0800

    Implement createWorkout api

--- frontend/mobile/src/api/workoutApi.ts
+++ frontend/mobile/src/api/workoutApi.ts
@@ -2,15 +2,34 @@ import {Workout} from '@src/types';
 import {ApiResponse} from './authApi';
 import {baseApi} from './baseApi';
 
-export const workoutApi = baseApi.injectEndpoints({
+export interface CreateWorkoutRequest {
+    name: string;
+}
+
+const taggedBaseApi = baseApi.enhanceEndpoints({addTagTypes: ['Workout']});
+
+export const workoutApi = taggedBaseApi.injectEndpoints({
     endpoints: (builder) => ({
-        listWorkouts: builder.query<ApiResponse<Workout[]>, string>({
-            query: (userId) => ({
-                url: `${userId}/workouts/`,
+        listWorkouts: builder.query<ApiResponse<Workout[]>, void>({
+            query: () => ({
+                url: ':userId/workouts/',
                 method: 'GET',
             }),
+            providesTags: (result) =>
+                result
+                    ? [...result.data.map(({id}) => ({type: 'Workout' as const, id})), {type: 'Workout', id: 'LIST'}]
+                    : [{type: 'Workout', id: 'LIST'}],
+        }),
+        createWorkout: builder.mutation<Workout, CreateWorkoutRequest>({
+            query: (workout) => ({
+                url: ':userId/workouts/',
+                method: 'POST',
+                body: workout,
+            }),
+            transformResponse: (response: ApiResponse<Workout>) => response.data,
+            invalidatesTags: [{type: 'Workout', id: 'LIST'}],
         }),
     }),
 });
 
-export const {useListWorkoutsQuery} = workoutApi;
+export const {useListWorkoutsQuery, useCreateWorkoutMutation} = workoutApi;
