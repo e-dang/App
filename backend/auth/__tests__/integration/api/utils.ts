759068cc2f1028add36ff2424a1fc33adc8dc342
Eric Dang <edang830@gmail.com>
2021-12-18 12:10:11 -0800

    Add extractCookies test utility

--- backend/auth/__tests__/integration/api/utils.ts
+++ backend/auth/__tests__/integration/api/utils.ts
@@ -11,3 +11,31 @@ export const createUser = async (app: Express, email: string, name: string, pass
         accessToken: res.body.data.accessToken,
     };
 };
+
+export interface Cookies {
+    [x: string]: {
+        value: string;
+        flags: {
+            [x: string]: string | boolean;
+        };
+    };
+}
+
+// https://gist.github.com/the-vampiire/a564af41ed0ce8eb7c30dbe6c0f627d8
+const shapeFlags = (flags: any) =>
+    flags.reduce((shapedFlags: any, flag: any) => {
+        const [flagName, rawValue] = flag.split('=');
+        // edge case where a cookie has a single flag and "; " split results in trailing ";"
+        const value = rawValue ? rawValue.replace(';', '') : true;
+        return {...shapedFlags, [flagName]: value};
+    }, {});
+
+export const extractCookies = (headers: any): Cookies => {
+    const cookies = headers['set-cookie']; // Cookie[]
+
+    return cookies.reduce((shapedCookies: any, cookieString: any) => {
+        const [rawCookie, ...flags] = cookieString.split('; ');
+        const [cookieName, value] = rawCookie.split('=');
+        return {...shapedCookies, [cookieName]: {value, flags: shapeFlags(flags)}};
+    }, {});
+};
