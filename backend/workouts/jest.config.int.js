fab91122cdcf7fb9e70e17b1d202998fb2493d13
Eric Dang <edang830@gmail.com>
2021-12-18 10:15:52 -0800

    Add globalSetup file to workouts for int testing

--- backend/workouts/jest.config.int.js
+++ backend/workouts/jest.config.int.js
@@ -9,5 +9,6 @@ module.exports = {
     setupFilesAfterEnv: ['./__tests__/integration/setup.ts'],
     moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {prefix: path.resolve(__dirname)}),
     testTimeout: process.env.CI ? 30000 : 10000,
+    globalSetup: './__tests__/integration/globalSetup.ts',
     runner: 'groups',
 };
