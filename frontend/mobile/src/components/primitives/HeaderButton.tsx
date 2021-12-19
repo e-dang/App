b59c9cd97ad78b47f8398e4e75a691ec1f819369
Eric Dang <edang830@gmail.com>
2021-12-19 14:47:34 -0800

    Add default themeing to HeaderButton

--- frontend/mobile/src/components/primitives/HeaderButton.tsx
+++ frontend/mobile/src/components/primitives/HeaderButton.tsx
@@ -3,7 +3,7 @@ import {Button, IButtonProps} from 'native-base';
 
 export const HeaderButton: FC<IButtonProps> = ({children, ...props}) => {
     return (
-        <Button {...props} px={0}>
+        <Button variant="ghost" colorScheme="primary" {...props} px={0}>
             {children}
         </Button>
     );
