db46d6c621388b5c62b550b779f9025fe04b098a
Eric Dang <edang830@gmail.com>
2021-12-19 14:09:26 -0800

    Implement BasicButton component

---
+++ frontend/mobile/src/components/primitives/BasicButton.tsx
@@ -0,0 +1,10 @@
+import React, {FC} from 'react';
+import {Button as NBButton, IButtonProps} from 'native-base';
+
+export const BasicButton: FC<IButtonProps> = ({children, ...props}) => {
+    return (
+        <NBButton borderRadius={100} {...props}>
+            {children}
+        </NBButton>
+    );
+};
--- frontend/mobile/src/components/primitives/index.ts
+++ frontend/mobile/src/components/primitives/index.ts
@@ -1,3 +1,4 @@
 export * from './Header';
 export * from './Screen';
 export * from './HeaderButton';
+export * from './BasicButton';
