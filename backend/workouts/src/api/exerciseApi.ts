fb9ae2f36f9dfba1a7d9bb4c974a48304b54d599
Eric Dang <edang830@gmail.com>
2021-12-18 16:38:21 -0800

    Implement create user exercise endpoint

--- backend/workouts/src/api/exerciseApi.ts
+++ backend/workouts/src/api/exerciseApi.ts
@@ -1,3 +1,4 @@
+import {validateCreateExerciseRequest} from '@schemas';
 import {validateIsOwner, verifyAuthN} from '@src/middleware';
 import {NextFunction, Response, Router} from 'express';
 import {ApiGroup, AuthenticatedRequest} from './types';
@@ -11,6 +12,16 @@ exerciseRouter.get(
     validateIsOwner,
     async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
         return res.status(200).json({data: await req.user.getSerializedExercises()});
+    },
+);
+
+exerciseRouter.post(
+    '/:userId/exercises',
+    validateIsOwner,
+    validateCreateExerciseRequest,
+    async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+        const exercise = await req.user.addExercise(req.body);
+        return res.status(201).json({data: exercise.serialize()});
     },
 );
 
