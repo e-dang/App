aafa2a80ef93d96c3b77eee97630c6e1452815e8
Eric Dang <edang830@gmail.com>
2021-12-19 12:23:59 -0800

    Add e2e test for adding exercises to workouts

--- frontend/mobile/__tests__/e2e/workouts.e2e.ts
+++ frontend/mobile/__tests__/e2e/workouts.e2e.ts
@@ -25,23 +25,49 @@ describe('create workout flow', () => {
 
         // the user taps the add workout button and is taken to the create workout screen
         await createWorkoutBtn.tap();
-        const createWorkoutFlowScreen = element(by.id('createWorkoutScreen'));
-        await expect(createWorkoutFlowScreen).toBeVisible();
+        const createWorkoutScreen = element(by.id('createWorkoutScreen'));
+        await expect(createWorkoutScreen).toBeVisible();
 
-        // the user then enters the name of the workout into the name input and taps the done button
+        // the user then enters the name of the workout into the name input
         const workoutName = 'My Test Workout';
         const workoutNameInput = element(by.id('nameInput'));
-        const doneBtn = element(by.id('doneBtn'));
-
         await expect(workoutNameInput).toBeVisible();
-        await expect(doneBtn).toBeVisible();
-
         workoutNameInput.typeText(workoutName);
         await expect(workoutNameInput).toHaveText(workoutName);
 
+        // then the user clicks the add exercise button
+        const addExerciseBtn = element(by.id('addExerciseBtn'));
+        await expect(addExerciseBtn).toBeVisible();
+        await addExerciseBtn.tap();
+
+        // a screen with a list of exercises to select from appear and the user selects multiple exercises
+        const exerciseListScreen = element(by.id('exerciseListScreen'));
+        await expect(exerciseListScreen).toBeVisible();
+
+        const exercises = ['Barbell Bench Press', 'Dumbbell Bench Press'];
+        for (let val of exercises) {
+            const exercise = element(by.text(val));
+            await expect(exercise).toBeVisible();
+            await exercise.tap();
+        }
+
+        // they finish their selection and can now see the exercises on the create workout screen
+        const finishAddExerciseBtn = element(by.text(`Add (${exercises.length})`));
+        await expect(finishAddExerciseBtn).toBeVisible();
+        await finishAddExerciseBtn.tap();
+        await expect(exerciseListScreen).not.toBeVisible();
+        await expect(createWorkoutScreen).toBeVisible();
+
+        for (let [idx, val] of exercises.entries()) {
+            const exerciseCard = element(by.id(`exerciseCard${idx}`).withDescendant(by.text(val)));
+            await expect(exerciseCard).toBeVisible();
+        }
+
+        // they save the workout and is taken back to the workout list screen where they see their new workout
+        const doneBtn = element(by.id('doneBtn'));
+        await expect(doneBtn).toBeVisible();
         await doneBtn.tap();
 
-        // the user is taken back to the workout list screen where they see their new workout
         await expect(listWorkoutsScreen).toBeVisible();
         await expect(element(by.text(workoutName))).toBeVisible();
     });
