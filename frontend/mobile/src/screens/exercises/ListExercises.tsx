2c76adabe7b98fa40ddae538396fcc6f7ccc5694
Eric Dang <edang830@gmail.com>
2021-12-18 16:18:19 -0800

    Create ListExerciseScreen

--- frontend/mobile/src/screens/exercises/ListExercises.tsx
+++ frontend/mobile/src/screens/exercises/ListExercises.tsx
@@ -1,5 +1,65 @@
-import React from 'react';
+import React, {FC} from 'react';
+import {Header, Screen} from '@components';
+import {Box, Button, Center, Heading, Spinner, Text} from 'native-base';
+import {useNavigation} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {ExerciseStackParamList} from '.';
+import {FlatList} from 'react-native-gesture-handler';
+import {useListExercisesQuery} from '@api';
+
+type ListExerciseNavProps = StackNavigationProp<ExerciseStackParamList, 'listExercises'>;
+
+const ExerciseScreen: FC = ({children}) => {
+    const navigation = useNavigation<ListExerciseNavProps>();
+
+    const handleCreateExercise = () => {
+        navigation.navigate('createExercise');
+    };
+
+    return (
+        <>
+            <Header />
+            <Screen testID="listExercisesScreen">
+                <Center flex={4} justifyContent="flex-start">
+                    <Heading>Exercises</Heading>
+                </Center>
+                <Center flex={3}>{children}</Center>
+                <Button
+                    testID="createExerciseBtn"
+                    variant="solid"
+                    colorScheme="primary"
+                    borderRadius={100}
+                    onPress={handleCreateExercise}>
+                    Create Exercise
+                </Button>
+            </Screen>
+        </>
+    );
+};
 
 export function ListExerciseScreen() {
-    return <></>;
+    const {data = {data: []}, isLoading} = useListExercisesQuery();
+
+    if (isLoading) {
+        return (
+            <ExerciseScreen>
+                <Spinner animating={isLoading} accessibilityLabel="Loading indicator" />
+            </ExerciseScreen>
+        );
+    }
+
+    return (
+        <ExerciseScreen>
+            <FlatList
+                testID="exerciseList"
+                data={data.data}
+                ListEmptyComponent={<Text>You Don't Have Any Exercises...</Text>}
+                renderItem={({item}) => (
+                    <Box>
+                        <Text>{item.name}</Text>
+                    </Box>
+                )}
+            />
+        </ExerciseScreen>
+    );
 }
