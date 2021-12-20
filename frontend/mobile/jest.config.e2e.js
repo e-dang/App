c632bb76b62a5dde361fd55179f6ec8b7b5e0b78
Eric Dang <edang830@gmail.com>
2021-12-19 16:19:28 -0800

    Enable path aliases in mobile tests

--- frontend/mobile/jest.config.e2e.js
+++ frontend/mobile/jest.config.e2e.js
@@ -1,8 +1,17 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+const {pathsToModuleNameMapper} = require('ts-jest');
+const JSON5 = require('json5');
+const path = require('path');
+const fs = require('fs');
+
+const tsconfig = JSON5.parse(fs.readFileSync(path.resolve('./tsconfig.json')));
+
 module.exports = {
     maxWorkers: 1,
     preset: 'ts-jest',
     testEnvironment: 'node',
     testTimeout: 120000,
+    moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {prefix: path.resolve(__dirname)}),
     testRegex: '__tests__/e2e/.*\\.e2e\\.ts$',
     setupFilesAfterEnv: ['./__tests__/e2e/setup.ts'],
 };
