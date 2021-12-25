bf0359b2e198473cb800a1388b5525fd64e32a8b
Eric Dang <edang830@gmail.com>
2021-12-25 12:27:41 -0800

    Fix quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
   root: true,
-  extends: ['airbnb', 'eslint:recommended', 'plugin:promise/recommended', 'plugin:prettier/recommended'],
+  extends: ["airbnb", "eslint:recommended", "plugin:promise/recommended", "plugin:prettier/recommended"],
   rules: {
-    'import/no-extraneous-dependencies': ['error', {devDependencies: true}],
+    "import/no-extraneous-dependencies": ["error", {devDependencies: true}],
   },
   overrides: [
     {
-      files: ['**/*.ts', '**/*.tsx'],
+      files: ["**/*.ts", "**/*.tsx"],
       extends: [
-        'airbnb-typescript',
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'plugin:jest/recommended',
-        'plugin:promise/recommended',
-        'plugin:prettier/recommended',
+        "airbnb-typescript",
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:jest/recommended",
+        "plugin:promise/recommended",
+        "plugin:prettier/recommended",
       ],
-      parser: '@typescript-eslint/parser',
+      parser: "@typescript-eslint/parser",
       env: {
         es2021: true,
         node: true,
@@ -24,16 +24,16 @@ module.exports = {
       },
       parserOptions: {
         ecmaVersion: 13,
-        sourceType: 'module',
+        sourceType: "module",
       },
       rules: {
-        'import/prefer-default-export': 'off',
-        '@typescript-eslint/no-unused-vars': [
-          'error',
-          {varsIgnorePattern: '^_', argsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_'},
+        "import/prefer-default-export": "off",
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          {varsIgnorePattern: "^_", argsIgnorePattern: "^_", caughtErrorsIgnorePattern: "^_"},
         ],
-        '@typescript-eslint/no-misused-promises': [
-          'error',
+        "@typescript-eslint/no-misused-promises": [
+          "error",
           {
             checksVoidReturn: false,
           },
