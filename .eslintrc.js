8a0f648b45aa780dc8a4963f1b5d3738bad81f3e
Eric Dang <edang830@gmail.com>
2021-12-25 11:39:41 -0800

    Update root eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,41 @@
 module.exports = {
-    root: true,
-    env: {
+  root: true,
+  extends: ['airbnb', 'eslint:recommended', 'plugin:promise/recommended', 'plugin:prettier/recommended'],
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      extends: [
+        'airbnb-typescript',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:jest/recommended',
+        'plugin:promise/recommended',
+        'plugin:prettier/recommended',
+      ],
+      parser: '@typescript-eslint/parser',
+      env: {
         es2021: true,
         node: true,
-    },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
+        jest: true,
+      },
+      parserOptions: {
         ecmaVersion: 13,
         sourceType: 'module',
+      },
+      rules: {
+        'import/prefer-default-export': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          {varsIgnorePattern: '^_', argsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_'},
+        ],
+        '@typescript-eslint/no-misused-promises': [
+          'error',
+          {
+            checksVoidReturn: false,
+          },
+        ],
+      },
     },
-    plugins: ['@typescript-eslint', 'prettier'],
-    rules: {
-        '@typescript-eslint/no-unused-vars': 'error',
-    },
+  ],
 };
