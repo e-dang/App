0af969820a95a1798ea062c04e0fa08472b5b6f7
Eric Dang <edang830@gmail.com>
2022-01-09 17:20:01 -0800

    Implement useSelectable hook

--- frontend/mobile/src/hooks/index.ts
+++ frontend/mobile/src/hooks/index.ts
@@ -3,3 +3,6 @@ import type {AppDispatch, RootState} from "@store";
 
 export const useSelector: TypedUseSelectorHook<RootState> = useReduxSelector;
 export const useDispatch = () => useReduxDispatch<AppDispatch>();
+
+export * from "./useSections";
+export * from "./useSelectable";
