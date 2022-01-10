b94f839fd93bb7fdea18f2b3cb7e98ca4bf2e97b
Eric Dang <edang830@gmail.com>
2022-01-10 15:41:58 -0800

    Add ExerciseTemplate and CreateExerciseTemplateForm

--- frontend/mobile/src/entities/exercise.ts
+++ frontend/mobile/src/entities/exercise.ts
@@ -2,6 +2,15 @@ import {Entity} from "./entity";
 
 export interface ExerciseType extends Entity {
   name: string;
-  createdAt: string;
-  lastUpdatedAt: string;
 }
+
+export interface ExerciseTemplateProperties {
+  targetReps: number;
+  targetSets: number;
+  targetWeight: number;
+  type: ExerciseType;
+}
+
+export interface ExerciseTemplate extends Entity, ExerciseTemplateProperties {}
+
+export type CreateExerciseTemplateForm = ExerciseTemplateProperties;
