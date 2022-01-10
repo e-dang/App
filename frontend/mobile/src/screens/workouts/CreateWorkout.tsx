af0ee30450a481881a49b0e3debb16bd773d367d
Eric Dang <edang830@gmail.com>
2022-01-10 15:45:02 -0800

    Handle adding exercises to workout template

--- frontend/mobile/src/screens/workouts/CreateWorkout.tsx
+++ frontend/mobile/src/screens/workouts/CreateWorkout.tsx
@@ -1,20 +1,43 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {BackButton, BasicButton, Header, HeaderButton, NameInput, Screen} from "@components";
-import {Box, Center, Heading, VStack, Icon as NBIcon, TextArea} from "native-base";
-import {useNavigation} from "@react-navigation/native";
+import {Box, Center, FlatList, Heading, VStack, Icon as NBIcon, TextArea, Text} from "native-base";
+import {useNavigation, useRoute, RouteProp} from "@react-navigation/native";
 import {StackNavigationProp} from "@react-navigation/stack";
-import {CreateWorkoutRequest, useCreateWorkoutMutation} from "@api";
+import {useCreateWorkoutTemplateMutation} from "@api";
 import Icon from "react-native-vector-icons/FontAwesome5";
 import {AppStackParamList} from "@screens/AppStack";
+import {ListRenderItem} from "react-native";
 import {logAsyncError} from "@utils";
+import {CreateExerciseGroupTemplateForm, CreateWorkoutTemplateForm, ExerciseType} from "@entities";
 import type {WorkoutStackParamList} from "./WorkoutStack";
 
 export type CreateWorkoutNavProps = StackNavigationProp<WorkoutStackParamList & AppStackParamList, "createWorkout">;
+export type CreateWorkoutRouteProps = RouteProp<WorkoutStackParamList, "createWorkout">;
 
 export const CreateWorkoutScreen = () => {
+  const route = useRoute<CreateWorkoutRouteProps>();
   const navigation = useNavigation<CreateWorkoutNavProps>();
-  const [createWorkout] = useCreateWorkoutMutation();
-  const [workoutForm, setWorkoutForm] = useState<CreateWorkoutRequest>({name: "", notes: ""});
+  const [createWorkout] = useCreateWorkoutTemplateMutation();
+  const [workoutForm, setWorkoutForm] = useState<CreateWorkoutTemplateForm>({name: "", notes: "", exerciseGroups: []});
+
+  useEffect(() => {
+    const addExerciseGroups = (exerciseTypes: ExerciseType[]) => {
+      setWorkoutForm((prev) => ({
+        ...prev,
+        exerciseGroups: [
+          ...prev.exerciseGroups,
+          ...exerciseTypes.map((type, idx) => ({
+            index: prev.exerciseGroups.length + idx,
+            exercises: [{targetReps: 0, targetSets: 0, targetWeight: 0, type}],
+          })),
+        ],
+      }));
+    };
+
+    if (route.params?.selectedExercises) {
+      addExerciseGroups(route.params.selectedExercises);
+    }
+  }, [route.params?.selectedExercises]);
 
   const handleBack = () => {
     navigation.navigate("listWorkouts");
@@ -28,6 +51,12 @@ export const CreateWorkoutScreen = () => {
   const handleAddExercises = () => {
     navigation.push("workoutAddExercises");
   };
+
+  const renderItem: ListRenderItem<CreateExerciseGroupTemplateForm> = ({item}) => (
+    <Box>
+      <Text>{item.exercises.map((exercise) => exercise.type.name)}</Text>
+    </Box>
+  );
 
   return (
     <Screen testID="createWorkoutScreen">
@@ -54,6 +83,19 @@ export const CreateWorkoutScreen = () => {
             value={workoutForm.notes}
           />
         </Box>
+        <Box>
+          <Text>Exercises</Text>
+        </Box>
+        <Center>
+          <FlatList
+            width="100%"
+            ListEmptyComponent={<Text>You Don&apos;t Have Any Exercises...</Text>}
+            keyExtractor={(item) => item.index as string}
+            data={workoutForm.exerciseGroups}
+            extraData={workoutForm.exerciseGroups}
+            renderItem={renderItem}
+          />
+        </Center>
         <Center>
           <BasicButton startIcon={<NBIcon as={Icon} name="plus" size={5} />} onPress={handleAddExercises}>
             Add Exercise
