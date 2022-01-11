ca094bca58dcce03308630962f91e25b6feb57cc
Eric Dang <edang830@gmail.com>
2022-01-10 20:10:03 -0800

    Add new entities to exports

--- backend/workouts/src/entities/index.ts
+++ backend/workouts/src/entities/index.ts
@@ -1,8 +1,12 @@
 import {User} from "./User";
 import {WorkoutTemplate} from "./WorkoutTemplate";
+import {ExerciseGroupTemplate} from "./ExerciseGroupTemplate";
+import {ExerciseTemplate} from "./ExerciseTemplate";
 import {ExerciseType} from "./ExerciseType";
 
-export const entities = [User, WorkoutTemplate, ExerciseType];
+export const entities = [User, WorkoutTemplate, ExerciseGroupTemplate, ExerciseTemplate, ExerciseType];
 export * from "./User";
 export * from "./WorkoutTemplate";
+export * from "./ExerciseGroupTemplate";
+export * from "./ExerciseTemplate";
 export * from "./ExerciseType";
