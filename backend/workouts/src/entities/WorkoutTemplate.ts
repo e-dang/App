9abdcbc13c66a04c3dedcfd4c386623ff20e2691
Eric Dang <edang830@gmail.com>
2022-01-10 20:08:25 -0800

    Implement ExerciseGroupTemplate entity

--- backend/workouts/src/entities/WorkoutTemplate.ts
+++ backend/workouts/src/entities/WorkoutTemplate.ts
@@ -6,13 +6,16 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  OneToMany,
 } from "typeorm";
+import {ExerciseGroupTemplate, ExerciseGroupTemplateDetail} from "./ExerciseGroupTemplate";
 import type {User} from "./User";
 
 export interface WorkoutTemplateDetail {
   id: string;
-  owner: string;
+  ownerId: string;
   name: string;
+  exerciseGroups: ExerciseGroupTemplateDetail[];
   createdAt: string;
   lastUpdatedAt: string;
 }
@@ -25,11 +28,14 @@ export class WorkoutTemplate extends BaseEntity {
   @Column({nullable: true})
   ownerId: string;
 
-  @ManyToOne("User", "workouts")
+  @ManyToOne("User", "workouts", {nullable: false})
   owner: User;
 
   @Column("varchar", {length: 255})
   name: string;
+
+  @OneToMany("ExerciseGroupTemplate", "workout", {cascade: true})
+  exerciseGroups: ExerciseGroupTemplate[];
 
   @Column({default: false})
   isDeleted: boolean;
@@ -40,13 +46,14 @@ export class WorkoutTemplate extends BaseEntity {
   @UpdateDateColumn({type: "timestamptz"})
   lastUpdatedAt: Date;
 
-  serialize(): WorkoutTemplateDetail {
+  serialize() {
     return {
       id: this.id,
-      owner: this.ownerId,
+      ownerId: this.ownerId,
       name: this.name,
+      exerciseGroups: this.exerciseGroups.map((group) => group.serialize()),
       createdAt: this.createdAt.toString(),
       lastUpdatedAt: this.lastUpdatedAt.toString(),
-    };
+    } as WorkoutTemplateDetail;
   }
 }
