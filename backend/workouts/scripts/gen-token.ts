fae7b7a067bd2f62a4d0a0f95ac9896ea12c4311
Eric Dang <edang830@gmail.com>
2022-02-05 12:48:10 -0800

    Parse dev private key from dev env file

--- backend/workouts/scripts/gen-token.ts
+++ backend/workouts/scripts/gen-token.ts
@@ -1,11 +1,19 @@
 /* eslint-disable no-console */
 import {randomUUID} from "crypto";
+import fs from "fs";
+import {load} from "js-yaml";
+import path from "path";
 import {createToken} from "../__tests__/integration/utils";
 
-// fix formatting of new lines from env vars
-process.env.accessTokenPrivateKey = process.env.accessTokenPrivateKey.replace(/\\n/g, "\n");
+function parseDevPrivateKey() {
+  const rawConfigs = load(
+    fs.readFileSync(path.join(__dirname, "..", ".env.dev.yaml"), {encoding: "utf-8"}).toString(),
+  ) as Record<string, string>;
+  return rawConfigs.accessTokenPrivateKey;
+}
 
 async function createUserAccessToken() {
+  process.env.accessTokenPrivateKey = parseDevPrivateKey();
   const userId = randomUUID();
   const token = await createToken({userId, role: "user"}, "1d");
   console.log();
