b03cb56b6215ecebbea394045ecfba5e893092eb
Eric Dang <edang830@gmail.com>
2022-02-21 13:50:38 -0800

    Add helmet middleware

--- backend/workouts/src/app.setup.ts
+++ backend/workouts/src/app.setup.ts
@@ -2,10 +2,12 @@ import {config} from "@config";
 import {INestApplication, ValidationPipe, VersioningType} from "@nestjs/common";
 import {NestFactory} from "@nestjs/core";
 import {Logger} from "nestjs-pino";
+import helmet from "helmet";
 import {AppModule} from "./app.module";
 
 export function appGlobalsSetup(app: INestApplication) {
   app.useLogger(app.get(Logger));
+  app.use(helmet());
   app.enableCors({origin: config.allowedHosts});
   app.setGlobalPrefix("api");
   app.enableVersioning({
