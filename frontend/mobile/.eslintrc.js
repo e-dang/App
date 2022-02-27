cbecd5c93fa493ad074ed452894b3a8f9edaf2d3
Eric Dang <edang830@gmail.com>
2022-02-27 09:50:04 -0800

    Update eslint config

--- frontend/mobile/.eslintrc.js
+++ frontend/mobile/.eslintrc.js
@@ -1,33 +1,21 @@
 const jestPackage = require("jest/package.json");
 
 module.exports = {
+  root: true,
+  extends: ["edang", "edang-react"],
   settings: {
     jest: {
       version: jestPackage.version,
     },
   },
-  overrides: [
-    {
-      files: ["**/*.ts", "**/*.tsx"],
-      plugins: ["react-native"],
-      env: {
-        "react-native/react-native": true,
-      },
-      parserOptions: {
-        project: `${__dirname}/tsconfig.json`,
-      },
-      rules: {
-        "react-native/no-unused-styles": "warn",
-        "react-native/split-platform-components": "error",
-        "react-native/no-inline-styles": "warn",
-        "react-native/no-color-literals": "warn",
-        // creates ugly code - just assign defaults in destructured props
-        "react/require-default-props": "off",
-        // how are you supposed to add inline icon props with this on?
-        "react/no-unstable-nested-components": "off",
-        // allow use of immer library from rtkquery
-        "no-param-reassign": [1, {props: true, ignorePropertyModificationsFor: ["state"]}],
-      },
-    },
-  ],
+  parserOptions: {
+    project: `${__dirname}/tsconfig.json`,
+  },
+  env: {
+    "react-native/react-native": true,
+  },
+  plugins: ["react-native"],
+  rules: {
+    "no-param-reassign": [1, {props: true, ignorePropertyModificationsFor: ["state"]}],
+  },
 };
